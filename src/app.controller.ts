71157d9f65e64731059463d93f938e4c831e67cf
Developer <pablogarcirodriguez@gmail.com>
2023-01-24 16:22:58 +0100

    Clases 7 y 8: Recibir parámetros y parámetros Query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -17,6 +17,33 @@ export class AppController {
 
   @Get('/ruta/')
   hello() {
-    return 'con /sas/';
+    return 'con /sas/ nueva ruta';
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 50,
+    @Query('brand') brand: string,
+  ): string {
+    return `Products: limit=> ${limit}, offset=> ${offset}, brand=> ${brand}`;
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return 'yo soy un filter';
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return `Product id: ${id}`;
+  }
+
+  @Get('categories/:id/products/:ID')
+  getCategory(@Param('id') id: string, @Param('ID') ID: string) {
+    return `
+    Categorie: ${id}
+    ID: ${ID}
+    `;
   }
 }
